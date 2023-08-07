fff419686a4fd5f39870d2e9403e9c955184d5ec
Mikepn02 <121401821+Mikepn02@users.noreply.github.com>
2023-08-07 23:54:29 +0200

    updated

--- OOP_Python/pop_push.py
+++ OOP_Python/pop_push.py
@@ -9,7 +9,7 @@ class Stack:
         if not self.is_empty():
             return self.items.pop()
         else:
-            return "Stack is empty"
+            return "Cannot pop from an empty stack"
     def is_empty(self):
         return len(self.items) == 0
 
@@ -35,3 +35,6 @@ print("Peek:", stack.peek())  # Output: 30
 print("Pop:", stack.pop())  # Output: 30
 
 print("Stack size:", stack.size())  # Output: 2
+
+# A stack is a fundamental data structure that provides a simple and efficient way to manage elements following the Last In First Out (LIFO) principle. It's commonly used in various algorithms, programming languages, and applications.
+
